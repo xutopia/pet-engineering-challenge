e2f64a4e81182f5bc462f1927014d82c35f7be13
Tony Xu <xuology@gmail.com>
2017-11-22 17:00:28 -0500

    complete brute force solution, need to output to csv file

--- solutions/bruteForce/index.js
+++ solutions/bruteForce/index.js
@@ -4,16 +4,38 @@ const fs = require('fs');
 
 /**
  * Brute force approach to determining the number of times pairs of doctors appear in multiple lists
- * @param  {File} csvFile
+ * @param {File} csvFile
  * @param {Number} occurences
  * @return {Array} pairs - list of pairs of doctors that match the criteria
  */
-const findPairsInListsByBruteForce = function(file, occurences) {
+const findPairsInListsByBruteForce = function(file, occurences = 40) {
   const data = stdinput(file);
   // console.log(data);
   let doctorPairs = {};
+  let pairs = [];
 
+  data.forEach(list => {
+    let listLength = list.length;
+    let sortedList = list.sort();
+    for (let i = 0; i < listLength - 1; i += 1) {
+      for (let j = i + 1; j < listLength; j += 1) {
+        let pair = `${sortedList[i]},${sortedList[j]}`;
+        if (doctorPairs[pair]) {
+          doctorPairs[pair] += 1;
+        } else {
+          doctorPairs[pair] = 1;
+        }
+      }
+    }
+  });
+
+  for (let doctors in doctorPairs) {
+    if (doctorPairs[doctors] >= occurences) {
+      pairs.push(doctors);
+    }
+  }
+  // console.log(pairs);
   return pairs;
 }
 
-// findPairsInListsByBruteForce('./care_teams.csv');
+findPairsInListsByBruteForce('./care_teams.csv');
